f7a9dcd367802e6bb10c208398e10e7c348b273d
alberthkyhky <131864802+alberthkyhky@users.noreply.github.com>
2023-12-15 22:56:48 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ ! -d pretrain ]; then
-    gdown https://drive.google.com/uc?id=1qlgYaHXzCLmt_EpdIIIc0i4qu83rfqhp -O pretrain.zip
+if [ ! -d model_weight ]; then
+    gdown https://drive.google.com/uc?id=1qlgYaHXzCLmt_EpdIIIc0i4qu83rfqhp -O model_weight.zip
 fi
 
-if [ ! -d pretrain ]; then
-    unzip pretrain.zip
+if [ ! -d model_weight ]; then
+    unzip model_weight.zip
 fi
