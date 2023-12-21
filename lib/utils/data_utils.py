589a34471680633584bbe47590ff9f10d438588b
Jia-Wei-Liao <sam23582211@gmail.com>
2023-12-21 20:05:04 +0800

    Move collate_func to data_utils.py

--- lib/utils/data_utils.py
+++ lib/utils/data_utils.py
@@ -1,4 +1,5 @@
 import json
+import torch
 
 
 def read_json(path: str) -> dict:
@@ -11,3 +12,15 @@ def write_json(data, path):
     with open(path, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
     return
+
+
+def collate_func(data: list) -> dict:
+    # convert list of dict to dict of list
+    data_list_dict = {k: [dic[k] for dic in data] for k in data[0]}
+
+    # convert dict of list to dict of torch tensor
+    data_tensor_dict = {
+        k: v if isinstance(v[0], str) else torch.tensor(v)
+        for k, v in data_list_dict.items()
+    }
+    return data_tensor_dict
