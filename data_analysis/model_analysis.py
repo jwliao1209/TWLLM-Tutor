01f62fd7cb855a2d408127acefb2ebf30371614d
jiawei <sam23582211@gmail.com>
2023-12-21 15:10:35 +0800

    Add model analysis

--- data_analysis/model_analysis.py
+++ data_analysis/model_analysis.py
@@ -10,7 +10,7 @@ from peft import LoraConfig, prepare_model_for_kbit_training, get_peft_model
 
 from configs import get_bnb_config
 from dataset import AcademicDataset, collate_func
-from optimization.optimizer import get_optimizer
+from optimizer import get_optimizer
 from trainer import Trainer
 from utils.train_utils import set_random_seeds
 from utils.data_utils import read_json
@@ -37,9 +37,6 @@ def parse_arguments() -> Namespace:
     parser.add_argument("--epoch", type=int,
                         default=1,
                         help="number of epochs")
-    parser.add_argument("--optimizer", type=str,
-                        default="adamw",
-                        help="optimizer")
     parser.add_argument("--lr", type=float,
                         default=2e-4,
                         help="learning rate")
@@ -69,28 +66,6 @@ if __name__ == "__main__":
 
     args = parse_arguments()
 
-    # Prepare dataset
-    tokenizer = AutoTokenizer.from_pretrained(args.base_model_path, use_fast=False)
-
-    train_data = read_json(args.train_data_path)
-    valid_data = read_json(args.valid_data_path)
-
-    train_dataset = AcademicDataset(
-        train_data, tokenizer,
-        max_length=512,
-        is_train=True,
-        with_answer_details=args.with_answer_details,
-    )
-    valid_dataset = AcademicDataset(
-        valid_data, tokenizer,
-        max_length=2048,
-        is_train=False,
-        with_answer_details=args.with_answer_details,
-    )
-
-    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, collate_fn=collate_func)
-    valid_loader = DataLoader(valid_dataset, batch_size=1, shuffle=False, collate_fn=collate_func)
-
     # Prepare model
     bnb_config = get_bnb_config()
     device = torch.device(f"cuda:{args.device_id}" if torch.cuda.is_available() else "cpu")
@@ -101,7 +76,6 @@ if __name__ == "__main__":
         
     )
     peft_config = LoraConfig(
-        target_modules=["q_proj", "v_proj"],
         lora_alpha=16,
         lora_dropout=0.1,
         r=args.lora_rank,
@@ -110,49 +84,4 @@ if __name__ == "__main__":
     )
     model = prepare_model_for_kbit_training(model)
     model = get_peft_model(model, peft_config)
-
-    # Prepared optimizer and learning rate scheduler
-    optimizer = get_optimizer(model, lr=args.lr, optimizer_name=args.optimizer, weight_decay=args.weight_decay)
-    num_update_steps_per_epoch = math.ceil(len(train_loader) / args.accum_grad_step)
-    max_train_steps = args.epoch * num_update_steps_per_epoch
-    lr_scheduler = get_scheduler(
-        name=args.lr_scheduler,
-        optimizer=optimizer,
-        num_warmup_steps=math.ceil(args.warm_up_step / args.accum_grad_step),
-        num_training_steps=max_train_steps,
-    )
-
-    # Prepared logger
-    wandb.init(
-        project="adl_final_project",
-        name="experiment", 
-        config={
-            "tokenizer": args.base_model_path,
-            "model": args.base_model_path,
-            "epoch": args.epoch,
-            "batch_size": args.batch_size,
-            "accum_grad_step": args.accum_grad_step,
-            "optimizer": "adamw",
-            "lr_scheduler": args.lr_scheduler,
-            "lr": args.lr,
-            "weight_decay": args.weight_decay,
-            "warm_up_step": args.warm_up_step,
-            "lora_rank": args.lora_rank,
-            "with_answer_details": args.with_answer_details,
-        }
-    )
-    wandb.watch(model, log="all")
-
-    # Start training
-    trainer = Trainer(
-        tokenizer=tokenizer,
-        model=model,
-        device=device,
-        train_loader=train_loader,
-        valid_loader=valid_loader,
-        optimizer=optimizer,
-        accum_grad_step=args.accum_grad_step,
-        lr_scheduler=lr_scheduler,
-        logger=wandb,
-    )
-    trainer.fit(epoch=args.epoch)
+    print(model["base_model"]["model"])
