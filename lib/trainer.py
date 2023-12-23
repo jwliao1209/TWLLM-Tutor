cb3f4176c105dfd3e7473dbc6804c47b3a67016f
JackLin <jacklin@cmlab.csie.ntu.edu.tw>
2023-12-23 12:20:29 +0800

    [F] Fix grad_scaler using bugs and add some methods

--- lib/trainer.py
+++ lib/trainer.py
@@ -23,23 +23,29 @@ class BaseTrainer:
         tokenizer,
         model,
         device,
-        train_loader,
-        valid_loader,
-        optimizer,
-        lr_scheduler,
+        train_loader=None,
+        valid_loader=None,
+        test_loader=None,
+        optimizer=None,
+        lr_scheduler=None,
         accum_grad_step: int = 1,
-        clip_grad_norm: Optional[float] = None,
+        clip_grad_norm: Optional[float] = 1.0,
         fp32: bool = False,
         logger=None,
         disable_valid_on_start: bool = False,
+        checkpoint_dir: Optional[str] = None,
+        prediction_dir: Optional[str] = None,
+        max_new_token: Optional[int] = None,
     ):
         self.tokenizer = tokenizer
         self.model = model
         self.device = device
         self.train_loader = train_loader
         self.valid_loader = valid_loader
-        self.train_num = len(train_loader)
-        self.valid_num = len(valid_loader)
+        self.test_loader = test_loader
+        self.train_num = len(train_loader) if train_loader is not None else 0
+        self.valid_num = len(valid_loader) if valid_loader is not None else 0
+        self.test_num = len(test_loader) if test_loader is not None else 0
         self.optimizer = optimizer
         self.accum_grad_step = accum_grad_step
         self.clip_grad_norm = clip_grad_norm
@@ -50,6 +56,9 @@ class BaseTrainer:
         self.grad_scaler = GradScaler(enabled=not fp32)
         self.logger = logger
         self.disable_valid_on_start = disable_valid_on_start
+        self.checkpoint_dir = checkpoint_dir if checkpoint_dir is not None else CHECKPOINT_DIR
+        self.prediction_dir = prediction_dir if prediction_dir is not None else PREDICTION_DIR
+        self.max_new_token = max_new_token if max_new_token is not None else MAX_NEW_TOKENS
 
     def train_step(self, batch_data, index):
         NotImplementedError
@@ -79,13 +88,14 @@ class BaseTrainer:
             self.progress_bar.set_postfix(self.tracker.result() | {"lr": self.lr_scheduler.get_last_lr()[0]})
             self.log(self.tracker.result() | {"lr": self.lr_scheduler.get_last_lr()[0]})
 
-            loss_to_backward = self.grad_scaler.scale(loss / self.accum_grad_step)
-            loss_to_backward.backward()
+            self.grad_scaler.scale(loss / self.accum_grad_step).backward()
 
             if step % self.accum_grad_step == 0:
+                self.grad_scaler.unscale_(self.optimizer)
                 if self.clip_grad_norm is not None:
                     nn.utils.clip_grad_norm_(self.model.parameters(), self.clip_grad_norm)
-                self.optimizer.step()
+                self.grad_scaler.step(optimizer=self.optimizer)
+                self.grad_scaler.update()
                 self.optimizer.zero_grad()
                 self.lr_scheduler.step()
 
@@ -114,8 +124,8 @@ class BaseTrainer:
         self.progress_bar.close()
 
         save_name = f"epoch={self.cur_ep}_acc={self.tracker.result().get('valid/acc', 0):.4f}"
-        write_json(pred_list, os.path.join(PREDICTION_DIR, f"{save_name}.json"))
-        self.model.save_pretrained(os.path.join(CHECKPOINT_DIR, save_name))
+        write_json(pred_list, os.path.join(self.prediction_dir, f"{save_name}.json"))
+        self.model.save_pretrained(os.path.join(self.checkpoint_dir, save_name))
 
     def fit(self, epoch):
         self.cur_ep = 0
@@ -124,6 +134,27 @@ class BaseTrainer:
         for self.cur_ep in range(1, epoch+1):
             self.train_one_epoch()
             self.valid_one_epoch()
+
+    def __repr__(self):
+        return (
+            f"{self.__class__.__name__}(\n"
+            f"\ttokenizer={self.tokenizer},\n"
+            f"\tmodel={self.model},\n"
+            f"\tdevice={self.device},\n"
+            f"\ttrain_loader={self.train_loader},\n"
+            f"\tvalid_loader={self.valid_loader},\n"
+            f"\ttrain_num={self.train_num},\n"
+            f"\tvalid_num={self.valid_num},\n"
+            f"\toptimizer={self.optimizer},\n"
+            f"\taccum_grad_step={self.accum_grad_step},\n"
+            f"\tclip_grad_norm={self.clip_grad_norm},\n"
+            f"\tlr_scheduler={self.lr_scheduler},\n"
+            f"\tdisable_valid_on_start={self.disable_valid_on_start},\n"
+            f"\tcheckpoint_dir={self.checkpoint_dir},\n"
+            f"\tprediction_dir={self.prediction_dir},\n"
+            f"\tmax_new_token={self.max_new_token},\n"
+            f")"
+        )
 
 
 class InstructionTuningTrainer(BaseTrainer):
@@ -143,7 +174,7 @@ class InstructionTuningTrainer(BaseTrainer):
         generated_tokens = self.model.generate(
             input_ids=batch_data["input_ids"],
             attention_mask=batch_data["attention_mask"],
-            max_new_tokens=MAX_NEW_TOKENS,
+            max_new_tokens=self.max_new_token,
         )
 
         generations = self.tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)[0]
@@ -162,15 +193,62 @@ class InstructionTuningTrainer(BaseTrainer):
         )
         return output_dict
 
+    @torch.no_grad()
+    def test(self, test_loader=None):
+        test_loader = self.test_loader if test_loader is None else test_loader
+        correct_num = 0
+        prediction_list = []
+        test_bar = tqdm(test_loader, desc=f"Testing")
+        for _, batch_data in enumerate(test_bar, start=1):
+            with torch.no_grad():
+                batch_data = dict_to_device(batch_data, self.device)
+                with torch.cuda.amp.autocast(dtype=torch.bfloat16):
+                    generated_tokens = self.model.generate(
+                        input_ids=batch_data["input_ids"],
+                        attention_mask=batch_data["attention_mask"],
+                        max_new_tokens=self.max_new_token,
+                    )
+                generations = self.tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)[0]
+                generations = generations.replace(batch_data["prompt"][0], "").strip()
+
+                is_correct = correcter(generations, batch_data['answer'][0], batch_data['answer_description'][0])
+                if is_correct:
+                    correct_num += 1
+
+                test_bar.set_postfix({"correct_num": correct_num})
+
+                print(f"Question:\n{batch_data['prompt'][0]}\n")
+                print(f"Answer:\n{batch_data['answer'][0]}\n")
+                print(f"Prediction:\n{generations}\n")
+                print(f"Is Correct: {is_correct}")
+
+                prediction_list.append(
+                    {
+                        "id": int(batch_data['id'][0]),
+                        "year": batch_data['year'][0],
+                        "prompt": batch_data['prompt'][0],
+                        "generation": generations,
+                        "answer": batch_data['answer'][0],
+                        "answer_details": batch_data['answer_description'][0],
+                        "is_correct": is_correct,
+                    }
+                )
+
+        print('Acc:', correct_num / len(test_loader))
+        return prediction_list
+
 
 class MCTrainer(BaseTrainer):
 
     def train_step(self, batch_data, index):
-        outputs = self.model(
-            input_ids=batch_data["input_ids"],
-            attention_mask=batch_data["attention_mask"],
-            labels=batch_data["labels"],
-        )
+        with torch.cuda.amp.autocast(
+            dtype=torch.bfloat16 if self.device.type == "cuda" and not self.fp32 else torch.float32
+        ):
+            outputs = self.model(
+                input_ids=batch_data["input_ids"],
+                attention_mask=batch_data["attention_mask"],
+                labels=batch_data["labels"],
+            )
         loss = outputs.loss
         preds = outputs.logits.argmax(dim=-1)
         self.tracker.update("train/loss", loss / preds.shape[0], preds.shape[0])
