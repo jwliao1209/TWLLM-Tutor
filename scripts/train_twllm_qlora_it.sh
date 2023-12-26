06b1b561816c96eb90dead2eed5b9579beead43f
jiawei <sam23582211@gmail.com>
2023-12-26 17:10:35 +0800

    Add yaml configs

--- scripts/train_twllm_qlora_it.sh
+++ scripts/train_twllm_qlora_it.sh
@@ -1,2 +1,2 @@
 CUDA_VISIBLE_DEVICES=0 ipython -- twllm_qlora/train_twllm.py \
-                                --config_path configs/twllm_qlora_IT-train_QB_history-valid_QB_history_w_answer_details.yaml
+                                --config_path configs/twllm_qlora_IT-train_GSAT_social-valid_GSAT_social.yaml
--- scripts/train_twllm_qlora_mc.sh
+++ scripts/train_twllm_qlora_mc.sh
@@ -1,2 +1,4 @@
-CUDA_VISIBLE_DEVICES=0 ipython -- twllm_qlora/train_twllm.py \
-                                --config_path configs/twllm_qlora_MC-train_QB_history-valid_QB_history.yaml
+CUDA_VISIBLE_DEVICES=1 ipython -- twllm_qlora/train_twllm.py \
+                                --config_path configs/twllm_qlora_MC-train_QB_history-valid_QB_history_wo_answer_details.yaml
+                                # --config_path configs/twllm_qlora_MC-train_GSAT_social-valid_GSAT_social.yaml
+                                # --config_path configs/twllm_qlora_MC-train_QB_history-valid_QB_history.yaml
