8be806dec15c3b3d375c65a0595a2d6d3787b8fe
072jiajia <072jiajia@gmail.com>
2023-12-28 00:11:36 +0800

    Add script for preparing visiual data

--- prepare_data/prepare_embeddings.py
+++ prepare_data/prepare_embeddings.py
@@ -6,11 +6,11 @@ import torch
 
 model, preprocess = clip.load("RN50")
 
-for filename in glob('./data/public/university_exams/social_study/*/*.png'):
+for filename in glob('./data/raw_data/GSAT_social/clean/*/*.png'):
     img = preprocess(Image.open(filename))
     year, id = filename.split("/")[-2:]
     id = id[:-4]
-    output_filename = f"./data/train_data_mc/embeddings/{year}_{id}.pth"
+    output_filename = f"./data/train_data/GSAT_social_with_image/embeddings/{year}_{id}.pth"
 
     emb = model.encode_image(img[None].cuda())
     emb = F.normalize(emb, p=2, dim=-1).cpu()
