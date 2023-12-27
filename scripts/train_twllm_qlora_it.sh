140ae0a2d92e794ed903959920459178dd7ffba8
Jia-Wei-Liao <sam23582211@gmail.com>
2023-12-28 00:43:04 +0800

    Add yaml config

--- scripts/train_twllm_qlora_it.sh
+++ scripts/train_twllm_qlora_it.sh
@@ -1,2 +1,2 @@
-CUDA_VISIBLE_DEVICES=1 ipython -- twllm/train_twllm.py \
-                                --config_path configs/twllm_qlora_MC-train_QB_history-valid_GSAT_history_w_answer_details.yaml
+CUDA_VISIBLE_DEVICES=0 ipython -- twllm/train_twllm.py \
+                                --config_path configs/twllm_qlora_IT-train_QB_history-valid_GSAT_history_w_answer_details.yaml
